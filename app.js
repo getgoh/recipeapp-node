c0b99bcce3bb8633e765e53f060442d3e867b60c
GET-PC-MSI\Get <agohca@gmail.com>
2019-03-06 17:19:41 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "recipedb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// get recipe by id
+app.get('/recipe/:id', function(req, res){
+    if (con)
+    {
+        queryStr = "select * from tbl_recipe where id=?";
+        con.query(queryStr, [req.params.id], function(err, rows){
+            if (err) throw err;
+            res.send(JSON.stringify(rows[0]));
+        });
+    }
+});
+
+// get all recipes
+app.get('/recipe', function(req, res){
+    if (con)
+    {
+        queryStr = "select * from tbl_recipe";
+        con.query(queryStr, function(err, rows){
+            if (err) throw err;
+            res.send(JSON.stringify(rows));
+        });
+    }
+});
+
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
