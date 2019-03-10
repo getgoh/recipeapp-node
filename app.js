3ebfd017449c511ee4d51bc827badb2e185b1dc6
GET-PC-MSI\Get <agohca@gmail.com>
2019-03-10 16:56:03 -0400

    Modified query for all recipes, directions now included

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ var fs = require("fs");
 var mysql = require('mysql');
 
 // var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "password",
-//   database: "recipedb"
+  
 // });
 
 
+  // var db_config = {
+  //   host: process.env.DB_HOST,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  //   database: process.env.DB_NAME
+  // };
+
   var db_config = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "recipedb"
   };
 
 function handleDisconnect() {
@@ -54,7 +58,22 @@ app.get('/recipe/:id', function(req, res){
         queryStr = "select * from tbl_recipe where id=?";
         con.query(queryStr, [req.params.id], function(err, rows){
             if (err) throw err;
-            res.send(JSON.stringify(rows[0], null, 4));
+
+            // recipeInfo = {};
+            // recipeInfo["recipe"] = rows[0];
+
+            dirsQueryStr = "select * from tbl_directions where recipe_id=?";
+            con.query(dirsQueryStr, [req.params.id], function(err, rows2){
+              if (err) throw err;
+                // recipeInfo["directions"] = rows2[0];
+                rows[0]["directions"] = rows2[0];
+
+
+                // res.send(JSON.stringify(rows[0], null, 4));
+                res.send(JSON.stringify(rows[0], null, 4));
+            });
+
+            
         });
     }
 });
@@ -63,10 +82,41 @@ app.get('/recipe/:id', function(req, res){
 app.get('/recipe', function(req, res){
     if (con)
     {
-        queryStr = "select * from tbl_recipe";
+        queryStr = "SELECT r.id, r.name, r.description, r.preptime, r.cookingtime, d.id as dir_id, d.recipe_id, d.directions \
+                    FROM recipedb.tbl_recipe r \
+                    inner join recipedb.tbl_directions d \
+                    on r.id = d.recipe_id;";
         con.query(queryStr, function(err, rows){
+
+            var result = [], recipes = {};
+
             if (err) throw err;
-            res.send(JSON.stringify(rows, null, 4));
+
+            rows.forEach(function (row) {
+              if ( !(row.id in recipes) ) {
+                console.log("log");
+                recipes[row.id] = {
+                      id: row.id,
+                      name: row.name,
+                      description: row.description,
+                      preptime: row.preptime,
+                      cookingtime: row.cookingtime,
+                      directions: []
+                  };
+                  result.push(recipes[row.id]);
+              }
+              recipes[row.id].directions.push({
+                  id: row.dir_id,
+                  recipe_id: row.recipe_id,
+                  directions: row.directions
+              });
+          });
+      
+          console.log(result);
+
+
+
+            res.send(JSON.stringify(result, null, 4));
         });
     }
 });
